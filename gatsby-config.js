376f442f1a2398c056e3ebf6ce311126defe2493
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-11-14 02:08:32 +0530

    connected to tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
-import ReactMarkdown from "react-markdown"
 
-export default function Home({ data }) {
-  // console.log(data.allMarkdownRemark.nodes)
-  const posts = data.allMarkdownRemark.nodes
-  return (
-    <div>
-      Hello world!
-      <br />
-      <h1>
-        <a href="/admin">admin</a>
-      </h1>
-      {posts.map((post, index) => {
-        return (
-          <article key={index}>
-            <h1>{post.frontmatter.title}</h1>
-            <p>{post.frontmatter.description}</p>
-            <ReactMarkdown>{post.rawMarkdownBody}</ReactMarkdown>
-          </article>
-        )
-      })}
-    </div>
-  )
+export default function Home() {
+  return <h1>Hello World</h1>
 }
-
-export const query = graphql`
-  {
-    allMarkdownRemark {
-      nodes {
-        frontmatter {
-          date
-          description
-          title
-        }
-        rawMarkdownBody
-      }
-    }
-  }
-`
